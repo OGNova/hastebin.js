6eec3924ded56124a590ce36c49e5f9548b55fe9
OGNova <jamooney2000@gmail.com>
2018-06-26 12:29:45 -0400

    Update get method.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { post } = require('snekfetch');
+const { post, get } = require('snekfetch');
 
 module.exports = class Hastebin {
   constructor() {
@@ -6,8 +6,18 @@ module.exports = class Hastebin {
   }
 
   async post(code) {
-    const { body } = await post("https://hastebin.com/documents").send(code).catch(console.error);
-    const url = `https://hastebin.com/${body.key}.js`
+    const { body } = await post('https://hastebin.com/documents').send(code).catch(console.error);
+    const url = `https://hastebin.com/${body.key}.js`;
     return url;
   }
-} 
+
+  async get(key) {
+    const url = `https://hastebin.com/${key}.js`;
+    const body = await get(url);
+    if (body.status === 200) {
+      return url;
+    } else {
+      throw new Error('That file does not exist. Please check your spelling and try again.');
+    }
+  }
+};
