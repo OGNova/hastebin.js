e69e543c73f6292b89190c6bfe50d915e0cb9a7b
OGNova <jamooney2000@gmail.com>
2018-09-02 21:49:12 -0400

    Final patch to TypeError bug.

--- index.js
+++ index.js
@@ -33,8 +33,15 @@ module.exports = class Hastebin {
   * @returns {Promise<pending>} 
   */
   async post(code) {
+    return this._post(code);
+  }
+
+  async _post(code) {
     if (!code) throw new Error('You must supply code to upload to hastebin.');
-    const res = await fetch(`${this.baseURL}/documents`, { method: 'POST', body: code });
+    const res = await fetch(`${this.baseURL}/documents`, {
+      method: 'POST',
+      body: code
+    });
     const json = await res.json();
     const url = `${this.baseURL}/${json.key}.js`;
     return url;
