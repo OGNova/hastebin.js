d2afcc0a89f56e6be1babb571fff0726ab3c773e
OGNovuh <jamooney2000@gmail.com>
2018-09-22 00:24:53 -0400

    Release 0.6.0 (#1)
    * Rewrite to use node-fetch.
    
    * Patch version.
    
    * Prep for v10 support.
    
    * Docs for intellisense.
    
    * Minor version bump.
    
    * Patch typo.
    
    * Version patch bump.
    
    * Possible patch.
    
    * Patch version bump
    
    * Attempted fix at TypeError: opts.slice...
    
    * Final patch to TypeError bug.
    
    * Documentation.
    
    * Fix docs.
    
    * Prep for typings.
    
    * Patch version bump.
    
    * New dev mode.
    
    * Bump version.
    
    * Bump version part 2, release time.

--- README.md
+++ README.md
@@ -2,13 +2,30 @@
 
 A NPM package to post data to hastebin.
 
+Due to how node-fetch works, you canot await the `post` function, you must use [#then](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then) to resolve the promise that's returned.
+
 ## Example Usage
 
 ```js
 const hastebin = require('hastebin.js');
 
-const haste = new hastebin();
+const haste = new hastebin({ /* dev: true, url: 'hastebin.com */ });
 
 const link = haste.post('Helllo from hastebin.js!').then(link => console.log(link));
 // Will return a link such as https://hastebin.com/sofomuqifo.js
 ```
+
+## Documentation
+
+* [Hastebin](#Hastebin)
+  * _instance_
+    * [.post(opts)](#Hastebin+post) ⇒ `{Promise<Pending>}`
+
+
+<a name="Hastebin+post"></a>
+
+### Hastebin.post
+
+| Param | Type | Description
+| --- | --- | --- |
+| opts | <code>string</code> | Required. The string that you want to post to Hastebin.
--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-const { get, post } = require('snekfetch');
+const fetch = require('node-fetch');
 
 module.exports = class Hastebin {
-  constructor() {}
+  /**
+   * @typedef {Object} ClientOptions
+   * @property {Boolean} [dev=false]
+   * @memberof Hastebin
+   */
 
-  async post(opts) {
-    const code = opts.splice(1).join(' ');
-    if (!code) throw new Error('You must supply code to upload to hastebin.');
-    const { body } = await post('https://hastebin.com/documents').send(code).catch(console.error);
-    const url = `https://hastebin.com/${body.key}.js`;
-    return url;
+  /**
+  * @param {ClientOptions} [options] Client options 
+  */
+  constructor(options = {}) {
+    /**
+    * Client options
+    * @type {object} 
+    */
+    this.options = options;
+    /**
+    * Whether or not to use hasteb.in
+    * @type {Boolean} 
+    */
+    this.dev = options.dev || false;
+    /**
+    * Base URL for Hastebin client.
+    * @type {string} 
+    */
+    this.baseURL = options.url || this.dev ? 'https://hasteb.in' : 'https://hastebin.com';
   }
 
-  async get(key) {
-    const url = `https://hastebin.com/${key}.js`;
-    const body = await get(url);
-    if (body.status === 200) {
-      return url;
-    } else {
-      throw new Error('That file does not exist. Please check your spelling and try again.');
-    }
+  /**
+  * @param {string} opts Code to post.
+  * @returns {Promise<pending>} 
+  */
+  async post(code) {
+    return this._post(code);
+  }
+
+  async _post(code) {
+    if (!code) throw new Error('You must supply code to upload to a haste service.');
+    const res = await fetch(`${this.baseURL}/documents`, {
+      method: 'POST',
+      body: code
+    });
+    const json = await res.json();
+    const url = `${this.baseURL}/${json.key}.js`;
+    return url;
   }
 };
